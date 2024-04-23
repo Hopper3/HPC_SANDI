7bf7db41227af3cc454d96f963b0a7bd53cf6b51
Hopper3 <107983852+Hopper3@users.noreply.github.com>
2024-04-23 11:06:30 -0400

    Update plt_figure.py

--- plt_figure.py
+++ plt_figure.py
@@ -8,12 +8,12 @@ speedup = [times[0] / time for time in times]
 cores_power_of_two = [2**i for i in range(6)]
 plt.figure(figsize=(10, 6))
 plt.plot(cores_power_of_two, speedup, marker='o', linestyle='-', color='b')
-plt.xscale('log', base=2)  # 使用base参数设置x轴为对数坐标，底数为2
+plt.xscale('log', base=2)  
 plt.xlabel('Number of Cores (log scale base 2)')
 plt.ylabel('Speedup')
 plt.title('Strong Scaling: Speedup vs Number of Cores (log scale, base 2)')
-plt.grid(True, which="both", ls="--")  # 对数网格
-plt.xticks(cores_power_of_two, labels=[f'$2^{i}$' for i in range(6)])  # 标记x轴为2的幂次形式
+plt.grid(True, which="both", ls="--")  
+plt.xticks(cores_power_of_two, labels=[f'$2^{i}$' for i in range(6)])  
 plt.yticks([round(s, 2) for s in speedup])
 plt.show()
 
